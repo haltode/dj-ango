f539f80db3c7ccea08d68038e148c709836fd2ed
Thibault Allançon <haltode@gmail.com>
2019-11-26 03:23:50 +0100

    cisco: add settings subfolder and split configuration
    Settings are now configured in: common.py, and dev.py (which can be
    written from the provided conf.sample.py).

---
+++ cisco/settings/conf.sample.py
@@ -0,0 +1,16 @@
+from .common import * # noqa
+
+
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# You can generate a key using the following command:
+# openssl rand -base64 64 | sed "s/[/10lO#+=]//g" | tr -d "\n"; echo
+SECRET_KEY = 'CHANGEME'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cisco',
+    }
+}
