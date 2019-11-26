2edc4df4ad7a2ad422d20460e31428da4920ae21
Thibault Allançon <haltode@gmail.com>
2019-11-26 03:45:16 +0100

    dj: add initial Song and Vote models

--- dj/admin.py
+++ dj/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Song, Vote
+
+admin.site.register(Song)
+admin.site.register(Vote)
--- dj/models.py
+++ dj/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    duration = models.DurationField()
+    is_playing = models.BooleanField(default=False)
+
+    yt_id = models.CharField(max_length=11, unique=True)
+    suggester = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} - {self.author} ({self.duration}s)'
+
+class Vote(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['song', 'user'], name='unique vote')
+        ]
+
+    def __str__(self):
+        return f'{self.user} voted for {self.song}'
