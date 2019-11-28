e6d57c1692ec64f8a24c75f46e0c4127ea999c8b
Thibault Allançon <haltode@gmail.com>
2019-11-28 18:04:47 +0100

    templates: mysongs: add list of liked songs

--- dj/views.py
+++ dj/views.py
@@ -20,9 +20,16 @@ def queue(request):
     context = {'songs': songs}
     return render(request, 'dj/queue.html', context)
 
+@login_required
 def mysongs(request):
     mines = Song.objects.filter(suggester=request.user)
-    context = {'songs': mines}
+    likes_ids = Like.objects.filter(user=request.user) \
+                    .exclude(song__in=mines).values('song')
+    likes = Song.objects.filter(pk__in=likes_ids)
+    context = {
+        'mines': mines,
+        'likes': likes,
+    }
     return render(request, 'dj/mysongs.html', context)
 
 def state(request):
