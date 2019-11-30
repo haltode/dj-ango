66a87ab50948c6d528f044e1c9e4ea639de1ed22
Thibault Allançon <haltode@gmail.com>
2019-11-30 17:48:26 +0100

    templates: add author and duration in song_list

--- dj/templatetags/utils.py
+++ dj/templatetags/utils.py
@@ -15,6 +15,16 @@ def elapsed_since(start):
     return timedelta(seconds=int(delta))
 
 @register.filter
+def format_duration(duration):
+    total_seconds = int(duration.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    if hours > 0:
+        return f'{hours}:{minutes}:{seconds}'
+    else:
+        return f'{minutes}:{seconds}'
+
+@register.filter
 def sort_songs_by_vote(songs):
     sorted_songs = sorted(songs, key=lambda s: get_nb_votes(s), reverse=True)
     return sorted_songs
